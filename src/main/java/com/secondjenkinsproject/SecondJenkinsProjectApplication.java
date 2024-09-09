49310e672c07be66d89eb41cb3de4f4ce5ea8444
sharathgitha627 <sharathgitha1627@gmail.com>
2024-09-09 22:46:20 +0530

    all files added please check

---
+++ src/main/java/com/secondjenkinsproject/SecondJenkinsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.secondjenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondJenkinsProjectApplication.class, args);
+		System.out.println("this is spring boot application....");
+	}
+
+}
---
+++ src/test/java/com/secondjenkinsproject/SecondJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondjenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

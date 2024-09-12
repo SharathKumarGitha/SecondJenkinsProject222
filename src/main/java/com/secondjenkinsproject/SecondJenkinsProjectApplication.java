7f54d76f12a4f8a8510d0d325a22b879e4f95874
sharathgitha627 <sharathgitha1627@gmail.com>
2024-09-12 14:50:36 +0530

    one line added in the main file

--- src/main/java/com/secondjenkinsproject/SecondJenkinsProjectApplication.java
+++ src/main/java/com/secondjenkinsproject/SecondJenkinsProjectApplication.java
@@ -9,6 +9,7 @@ public class SecondJenkinsProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecondJenkinsProjectApplication.class, args);
 		System.out.println("this is spring boot application....");
+		System.out.println("this is sample application....");
 	}
 
 }
